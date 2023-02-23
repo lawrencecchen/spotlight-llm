bfde331d7409a05c32bad3c4c02bbdaf8605b4e7
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2023-02-22 16:13:24 -0800

    update dates

--- server/router.ts
+++ server/router.ts
@@ -53,10 +53,15 @@ Your contacts:
 - Kaushik Patnaik kaushik@minion.ai
 - Subhash Ramesh subhash@minion.ai
 
+Calendar convetions:
+Monday = 0
+...
+Sunday = 6
+
 Example 1:
 Task: invite subhash and kaushik to dinner on friday night
 Context:
-app.currentDate() == Mon, 22 Feb 2023 22:29:10 GMT
+app.currentDate() == Monday, February 20, 2023 at 4:11:25 PM
 Output:
 \`\`\`js
 let app = Application.currentApplication()
@@ -64,9 +69,9 @@ app.includeStandardAdditions = true
 let Calendar = Application("Calendar")
 
 let eventStart = app.currentDate()
-// add days until friday. 0 = today, 1 = tomorrow, etc. Since today is Monday, we want to add 4 days.
-const targetDate = eventStart.getDate() + 4;
-eventStart.setDate(targetDate)
+let today = app.currentDate()
+const startDate = eventStart.getDate() - today.getDay() + 5;
+eventStart.setDate(startDate)
 eventStart.setHours(17)
 eventStart.setMinutes(0)
 eventStart.setSeconds(0)
@@ -87,10 +92,10 @@ sendReply("Ok! I scheduled dinner for you on Friday at 5pm, and sent an invitati
 \`\`\`
 
 
-Example 1:
+Example 2:
 Task: clear all events for this week
 Context:
-app.currentDate() == Mon, 22 Feb 2023 22:29:10 GMT
+app.currentDate() == Monday, February 20, 2023 at 4:11:25 PM
 Output:
 \`\`\`js
 let app = Application.currentApplication()
@@ -126,19 +131,55 @@ Calendar.reloadCalendars()
 sendReply("Ok! I cleared all of your events for this week.")
 \`\`\`
 
+Example 3:
+Task: invite subhash to dinner from thursday to the rest of the week
+Context:
+app.currentDate() == Monday, February 20, 2023 at 4:11:25 PM
+Output:
+\`\`\`js
+let app = Application.currentApplication()
+app.includeStandardAdditions = true
+let Calendar = Application("Calendar")
+
+let projectCalendars = Calendar.calendars.whose({name: "Home"})
+let projectCalendar = projectCalendars[0]
+
+// add dinner events for thursday, friday, saturday, and sunday
+// since today is monday and we want to start on thursday and end on sunday, the index starts at 4 and ends at 6
+for (let i = 4; i <= 6; i++) {
+  let today = app.currentDate()
+  let eventStart = app.currentDate()
+  const startDate = today.getDate() - today.getDay() + i;
+  eventStart.setDate(startDate)
+  eventStart.setHours(17)
+  eventStart.setMinutes(0)
+  eventStart.setSeconds(0)
+  let eventEnd = new Date(eventStart.getTime())
+  eventEnd.setHours(18)
+
+  let event = Calendar.Event({summary: "Dinner with Subhash", startDate: eventStart, endDate: eventEnd})
+  projectCalendar.events.push(event)
+  event.attendees.push(Calendar.Attendee({email: "subhash@minion.ai"}))
+}
+
+Calendar.reloadCalendars()
+
+sendReply("Ok! I scheduled dinner on Thursday, Friday, Saturday, and Sunday at 5pm. I've also sent an invitation to Subhash on all 4 days.")
+\`\`\`
+
 
 Begin.
 Task: ${input.message}
 Context:
-app.currentDate() == ${new Date().toUTCString()}
+app.currentDate() == ${new Date().toLocaleString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })}
 Output:
 \`\`\`js`
-      // console.log(prompt);
+      console.log(prompt);
       const completion = await openai.createCompletion({
         model: 'text-davinci-003',
         prompt,
         stop: ["```"],
-        max_tokens: 500,
+        max_tokens: 700,
         temperature: 0
       })
       const text = completion.data.choices[0].text || ""
